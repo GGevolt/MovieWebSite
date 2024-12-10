75c7e2251c52937b7759ebd857eb2be4aa3b5019
Nguyễn Văn Bảo Duy <117926234+GGevolt@users.noreply.github.com>
2024-12-11 00:36:39 +0700

    Fix bug

--- moviewebsite.client/src/Website/Area/User/Components/DisplayFilm/CarouselDisplay.jsx
+++ moviewebsite.client/src/Website/Area/User/Components/DisplayFilm/CarouselDisplay.jsx
@@ -2,27 +2,29 @@ import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import FilmImg from "../../Components/Img/FilmImg";
 import { Carousel } from "react-bootstrap";
-import "./Carousel.css";
+import styles from './CarouselDisplay.module.css';
 
 export default function CarouselDisplay({ films }) {
   const navigate = useNavigate();
   return (
-    <div className="film-carousel-container">
+    <div className={styles.filmCarouselContainer}>
       <Carousel interval={null}>
         {films.map((film) => (
-          <Carousel.Item key={film.id} onClick={() => navigate(`/user/detail/${film.id}`)}>
-            <div className="row">
-              <div className="col film-info">
-                <h2>{film.title}</h2>
-                <h3>Directed by {film.director}</h3>
-                <p>{film.synopsis}</p>
-              </div>
-              <div className="col film-image">
-                <FilmImg
-                  src={`/api/images/${film.filmPath}`}
-                  hash={film.blurHash}
-                />
-              </div>
+          <Carousel.Item 
+            key={film.id} 
+            onClick={() => navigate(`/user/detail/${film.id}`)}
+            className={styles.carouselItem}
+          >
+            <div className={styles.filmImage}>
+              <FilmImg
+                src={`/api/images/${film.filmPath}`}
+                hash={film.blurHash}
+              />
+            </div>
+            <div className={styles.filmInfo}>
+              <h2>{film.title}</h2>
+              <h3>Directed by {film.director}</h3>
+              <p className={styles.description}>{film.synopsis}</p>
             </div>
           </Carousel.Item>
         ))}
@@ -39,7 +41,9 @@ CarouselDisplay.propTypes = {
       synopsis: PropTypes.string,
       director: PropTypes.string,
       type: PropTypes.string,
-      filmImg: PropTypes.string,
+      filmPath: PropTypes.string,
+      blurHash: PropTypes.string,
     })
   ).isRequired,
 };
+
