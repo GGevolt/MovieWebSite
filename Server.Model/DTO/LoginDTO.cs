6d7742e7817fd174767f9591edbd31be08008cdf
Nguyễn Văn Bảo Duy <117926234+GGevolt@users.noreply.github.com>
2025-04-27 17:08:03 +0700

    delete unnecessary code

--- Server.Model/DTO/LoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Server.Model.DTO
-{
-    public class LoginDTO
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool RememberMe { get; set; }
-    }
-}
